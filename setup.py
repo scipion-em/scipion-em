fdeed0ca798f7ad2d005162ffb1d4f8bd0aba5f3
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2019-09-17 17:15:00 +0200

    * Add some needed plugin files

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='scipion-em',  # Required
+    version='1.0.0',  # Required
+    description='This modules contains classes related with EM',  # Required
+    long_description=long_description,  # Optional
+    url='https://github.com/scipion-em/scipion-em',  # Optional
+    author='I2PC',  # Optional
+    author_email='scipion@cnb.csic.es',  # Optional
+    keywords='scipion cryoem imageprocessing scipion-2.0',  # Optional
+    packages=find_packages(),
+    # package_data={  # Optional
+    # #    '': [''],
+    # }
+)
