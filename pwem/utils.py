9014b75031d429f04dea923fd2213273ec3b051c
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2019-09-17 17:13:45 +0200

    * First version of scipion-em as a plugin   - Deleted several method that are now in pyworkflow   - Moved other methods to utils.py

---
+++ pwem/utils.py
@@ -0,0 +1,57 @@
+# **************************************************************************
+# *
+# * Authors: Yunior C. Fonseca Reyna    (cfonseca@cnb.csic.es)
+# *
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+import pyworkflow.plugin as pwplugin
+import pyworkflow.utils as pwutils
+
+
+def loadSetFromDb(dbName, dbPrefix=''):
+    from pyworkflow.mapper.sqlite import SqliteFlatDb
+    db = SqliteFlatDb(dbName=dbName, tablePrefix=dbPrefix)
+    setClassName = db.getProperty('self') # get the set class name
+    setObj = pwplugin.Domain.getObjects()[setClassName](filename=dbName, prefix=dbPrefix)
+    return setObj
+
+
+def runProgram(program, params):
+    env = None
+
+    if program.startswith('xmipp'):
+        xmipp3 = pwplugin.importFromPlugin('xmipp3', 'Plugin')
+        env = xmipp3.getEnviron()
+    if program.startswith('relion'):
+        relion = pwplugin.importFromPlugin('relion', 'Plugin')
+        env = relion.getEnviron()
+    elif (program.startswith('e2') or
+              program.startswith('sx')):
+        eman2 = pwplugin.importFromPlugin('eman2', 'Plugin')
+        env = eman2.getEnviron()
+    elif program.startswith('b'):
+        bsoft = pwplugin.importFromPlugin('bsoft', 'Plugin')
+        env = bsoft.getEnviron()
+
+    pwutils.runJob(None, program, params, env=env)
