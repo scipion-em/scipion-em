de56bb9dd32873fa0f626248425dc16a8950ad1f
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2020-01-21 14:03:11 +0100

    Extend Acquisition class

--- pwem/objects/data.py
+++ pwem/objects/data.py
@@ -67,10 +67,14 @@ class Acquisition(EMObject):
         self._doseInitial = Float(kwargs.get('doseInitial', 0))
         self._dosePerFrame = Float(kwargs.get('dosePerFrame', None))
 
+        self.opticsGroupName = String()
+        self.beamTiltX = Float()
+        self.beamTiltY = Float()
+        self.mtfFile = String()
+        self.defectFile = String()
+
     def copyInfo(self, other):
-        self.copyAttributes(other, '_magnification', '_voltage',
-                            '_sphericalAberration', '_amplitudeContrast',
-                            '_doseInitial', '_dosePerFrame')
+        self.copy(other, copyId=False)
 
     def getMagnification(self):
         return self._magnification.get()
