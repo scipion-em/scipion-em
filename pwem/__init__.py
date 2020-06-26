1f04e174983437af1561caaa0f4c8a6a7c37e08b
pconesa <p.conesa.mingo@gmail.com>
2020-06-26 15:59:16 +0200

    Version bumped. Version taken from pwem.__version__ Entry points taken from constants. This requires using twine within an environment where pyworkflow exists.

--- pwem/__init__.py
+++ pwem/__init__.py
@@ -38,7 +38,7 @@ import pyworkflow.plugin
 from .constants import *
 from .objects import EMObject
 from .utils import *
-
+__version__ = '3.0.1b2'
 _logo = "scipion_icon.gif"
 _references = ["delaRosaTrevin201693"]
 
