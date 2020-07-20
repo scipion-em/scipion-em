f84c87e444ea774e980e083590c0b98cb25ee677
pconesa <p.conesa.mingo@gmail.com>
2020-07-20 14:21:34 +0200

    version bumped

--- pwem/__init__.py
+++ pwem/__init__.py
@@ -38,7 +38,7 @@ import pyworkflow.plugin
 from .constants import *
 from .objects import EMObject
 from .utils import *
-__version__ = '3.0.1b3'
+__version__ = '3.0.1'
 _logo = "scipion_icon.gif"
 _references = ["delaRosaTrevin201693"]
 
