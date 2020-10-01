ecf9be8706ce63c518edf9f61e24d2f81bd238e0
JorMaister <jorgejjs86@gmail.com>
2020-10-01 12:38:35 +0200

    Improve correction code

--- pwem/wizards/wizard.py
+++ pwem/wizards/wizard.py
@@ -560,7 +560,7 @@ class DownsampleDialog(ImagePreviewDialog):
         downButton.grid(row=1, column=1, padx=5, pady=5, sticky='ew')
 
     def getDownsample(self):
-        return 1
+        return float(self.downVar.get())
 
     def manageMaskVals(self):
         # To be coded by each child, if necessary
@@ -705,8 +705,8 @@ class CtfDialog(DownsampleDialog):
         # Update both mask and sliders with the initial values
         self.manageMaskVals()
 
-    # def getDownsample(self):
-    #     return 1.0  # Micrograph previously downsample, not taken into account here
+    def getDownsample(self):
+        return 1.0  # Micrograph previously downsample, not taken into account here
 
     def updateFreqRing(self):
         self.rightPreview.updateFreq(self.getLowFreq(), self.getHighFreq())
