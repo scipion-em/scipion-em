0b3a520746c55f55edacdbc4e5fae1da65498858
rmarabini <roberto@cnb.csic.es>
2021-07-16 17:15:27 +0200

    Update __init__.py

--- pwem/__init__.py
+++ pwem/__init__.py
@@ -55,7 +55,7 @@ class Config(pw.Config):
     XMIPP_HOME = _join(_get('XMIPP_HOME', os.path.join(EM_ROOT, 'xmipp')))
 
     # Needed by Chimera viewer: (TODO: Take the value from the plugin)
-    CHIMERA_HOME = _join(_get('CHIMERA_HOME', os.path.join(EM_ROOT, 'chimerax-1.2.5')))
+    CHIMERA_HOME = _join(_get('CHIMERA_HOME', os.path.join(EM_ROOT, 'chimerax-1.1')))
 
     # Get java home, we might need to provide correct default value
     JAVA_HOME = _get('JAVA_HOME', '')
