126925f89770c5e91408988dcc25a274272f58d3
DaniDelHoyo <danieldelhoyog@gmail.com>
2022-05-11 17:20:52 +0200

    variable wizard: new functionality to specify inputParam based on another param

--- pwem/wizards/wizard.py
+++ pwem/wizards/wizard.py
@@ -159,8 +159,34 @@ class VariableWizard(pwizard.Wizard):
         for target in self._targets:
             if form.wizParamName == target[1][0] and form.protocol.__class__ == target[0]:
                 prot, target = (target[0], target[1][0])
-                inParam, outParam = self._inputs[(prot, target)], self._outputs[(prot, target)]
-        return inParam, outParam
+                inParams, outParam = self._inputs[(prot, target)], self._outputs[(prot, target)]
+                inParams = self.filterPresentInputs(inParams, form.protocol)
+        return inParams, outParam
+
+    def filterPresentInputs(self, inputParams, protocol):
+        '''Filter the inputs to allow flexibility:
+        1) InputParams can be lists: the first not None parameter of the list will be chosen.
+        2) InputParams can be dict: key -> EnumParam (or IntParam) which functions as index
+                                    value -> list containing paramNames. The hey index element will be chosen
+        '''
+        fInpParams = []
+        for inPar in inputParams:
+            if type(inPar) == list:
+                present = ''
+                for option in inPar:
+                    if getattr(protocol, option).get():
+                        present = option
+                        break
+                fInpParams.append(present)
+            elif type(inPar) == dict:
+                idxParam = list(inPar.keys())[0]
+                optionParams = inPar[idxParam]
+
+                idx = getattr(protocol, idxParam).get()
+                fInpParams.append(optionParams[idx])
+            else:
+                fInpParams.append(inPar)
+        return fInpParams
 
 class DownsampleWizard(EmWizard):
 
