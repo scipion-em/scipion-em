ceb2d36444f167a3276c4bdf8d9692a0fc70df37
Yunior C. Fonseca Reyna <cfonseca@cnb.csic.es>
2022-09-07 17:37:57 +0200

    * Updating CHANGES.txt again

--- pwem/__init__.py
+++ pwem/__init__.py
@@ -41,7 +41,7 @@ from .objects import EMObject
 from .tests import defineDatasets
 from .utils import *
 
-__version__ = '3.0.22'
+__version__ = '3.0.21'
 NO_VERSION_FOUND_STR = "0.0"
 CUDA_LIB_VAR = 'CUDA_LIB'
 
