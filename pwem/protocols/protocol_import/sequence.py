62ef136d8e5f156c223e26547d974d11622e728a
DaniDelHoyo <danieldelhoyog@gmail.com>
2025-09-29 17:09:28 +0200

    Check not none

--- pwem/protocols/protocol_import/sequence.py
+++ pwem/protocols/protocol_import/sequence.py
@@ -500,10 +500,12 @@ class ProtImportSequence(ProtImportFiles):
         pass
 
     def _getUniProtID(self):
-        return self.uniProtSequence.get().strip()
+        uniId = self.uniProtSequence.get()
+        return uniId.strip() if uniId else uniId
 
     def _getGeneBankID(self):
-        return self.geneBankSequence.get().strip()
+        genId = self.geneBankSequence.get()
+        return genId.strip() if genId else genId
 
     def _getAlphabet(self):
         if self.inputSequence == Alphabet.AMINOACIDS:
